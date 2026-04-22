d14cb7e3c4d6cbe1e7fe63d0ff181603e36f7636
max13h <contact@hmae.fr>
2026-04-22 09:57:06 +0200

    🔥 Remove test because it's not the moment

--- tests/mocks/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./obsidian";
-export * from "./settings";
-export * from "./state";
-export * from "./webdav";
--- tests/mocks/state.ts
+++
@@ -1,5 +0,0 @@
-import type { SyncState } from "../../src/sync/state";
-
-export function makeState(files: SyncState["files"] = {}): SyncState {
-	return { lastSync: 0, files };
-}
